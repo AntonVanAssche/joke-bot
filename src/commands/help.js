e9fd23bc019e917c947f9c454f7c022cce86b7d6
Anton <vanasscheanton@gmail.com>
2023-04-17 20:43:15 +0200

    Refactor list of commands into individual lines
    The previous list of commands in help.js was in a single line, making it hard to read and modify. This commit refactors the list into individual lines, making it more readable and easier to add or remove commands in the future. The new list includes three new commands: '8ball', 'how-gay' and 'lovemeter'.

--- src/commands/help.js
+++ src/commands/help.js
@@ -10,7 +10,18 @@ module.exports = {
             .setDescription(`Prefix : ${prefix}`)
             .addField(
                 `List of commands :`,
-                "`help`: Show this help message.\n`ping`: Check the bot's ping.\n`stats`: Check the bot's stats.\n`joke`: Get a random joke.\n`category`: Get a random joke from a specific category.\n`pickup-line`: Get a random pickup line.\n`insult`: Get insulted!\n`pp-size`: Let everyone kow how big your pp is.\n`meme`: Get a random meme from Reddit.\n"
+                "`help`: Show this help message.\n" +
+                    "`ping`: Check the bot's ping.\n" +
+                    "`stats`: Check the bot's stats.\n" +
+                    "`joke`: Get a random joke.\n" +
+                    "`category`: Get a random joke from a specific category.\n" +
+                    "`pickup-line`: Get a random pickup line.\n" +
+                    "`insult`: Get insulted!\n" +
+                    "`pp-size`: Let everyone kow how big your pp is.\n" +
+                    "`how-gay`: Let everyone know how _gay_ you are.\n" +
+                    "`lovemeter`: Let everyone know how much you love someone.\n" +
+                    "`8ball`: Ask the magic 8ball a question.\n" +
+                    "`meme`: Get a random meme from Reddit.\n"
             )
             .addField(
                 `List of categories :`,
